206bf46e932c647dad979422ad2036424c011474
Monaliisa <mmss@cin.ufpe.br>
2023-03-21 12:19:24 -0300

    arquivos desafio 2

---
+++ desafio2/main.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+const mensagem = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert(mensagem);
